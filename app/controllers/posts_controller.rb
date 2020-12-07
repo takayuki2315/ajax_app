cbc43566efb67d2fc16b18c11d71af341f99d227
takayuki2315 <t.s151515@icloud.com>
2020-12-07 20:16:27 +0900

    既読機能のフロントエンドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,13 @@ class PostsController < ApplicationController
 
   def checked
     post = Post.find(params[:id])
-    if post.checksed
+    if post.checked
       post.update(checked: false)
     else
       post.update(checked: true)
     end
 
     item = Post.find(params[:id])
-    render json: { post: item}
+    render json: { post: item }
   end
 end
